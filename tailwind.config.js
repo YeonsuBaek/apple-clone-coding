ea126cb2326cc9143c97f5bfc3e0fe375b001a88
yeonsuBaek <soothbugger@gmail.com>
2023-01-28 08:45:47 +0900

    Style: Global 스타일 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".gradient": {
+          backgroundImage: `linear-gradient(
+              180deg,
+              #ffb6ff,
+              #b344ff 10%,
+              #ae38ff 33%,
+              #ffb6ff 45%,
+              #ffe3ff 50%,
+              #ffb6ff 66%,
+              #b344ff
+            )`,
+        },
+        ".transition": "duration-200 ease-in-out",
+      });
+    }),
+  ],
 };
