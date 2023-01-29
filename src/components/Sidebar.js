60f580efc957658ec8e359b5bc6036c77ced3956
YeonsuBaek <soothbugger@gmail.com>
2023-01-29 21:13:05 +0900

    feat: Sidebar 컴포넌트의 Global Nav 구현

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Sidebar() {
   return (
-    <aside className="absolute left-0 w-screen h-auto bg-black top-12">
-      <div className="px-2.5 h-[53px] w-full flex items-start justify-center border-b border-[#424245]">
+    <aside className="absolute left-0 w-screen h-[calc(100vh-48px)] bg-black top-12">
+      <div className="mb-1 px-2.5 h-[53px] w-full flex items-start justify-center border-b border-[#424245]">
         <form className="relative mx-auto my-0 w-full max-w-[584px]">
           <button className="focus absolute top-0 left-0 w-[30px] h-9 bg-no-repeat bg-left-[8px] bg-center bg-[url('https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_search_image__cbllq1gkias2_small.svg')]"></button>
           <input
@@ -12,6 +12,61 @@ function Sidebar() {
             className="pl-[30px] rounded-lg w-full block h-9 bg-[#1d1d1f] outline-0 text-[17px] placeholder:font-['SF_Pro_Text'] leading-[1.23536] tracking-[0.022em] placeholder:text-[#6e6e73]"
           />
         </form>
+      </div>
+
+      <div className="px-10">
+        <ul className="max-w-[584px] mx-auto my-0">
+          <li className="h-11 text-[17px] border-b border-[#424245]">
+            <a href="/" className="flex items-center w-full h-full">
+              Store
+            </a>
+          </li>
+          <li className="h-11 text-[17px] border-b border-[#424245]">
+            <a href="/" className="flex items-center w-full h-full">
+              Mac
+            </a>
+          </li>{' '}
+          <li className="h-11 text-[17px] border-b border-[#424245]">
+            <a href="/" className="flex items-center w-full h-full">
+              iPad
+            </a>
+          </li>
+          <li className="h-11 text-[17px] border-b border-[#424245]">
+            <a href="/" className="flex items-center w-full h-full">
+              iPhone
+            </a>
+          </li>
+          <li className="h-11 text-[17px] border-b border-[#424245]">
+            <a href="/" className="flex items-center w-full h-full">
+              Watch
+            </a>
+          </li>{' '}
+          <li className="h-11 text-[17px] border-b border-[#424245]">
+            <a href="/" className="flex items-center w-full h-full">
+              AirPods
+            </a>
+          </li>{' '}
+          <li className="h-11 text-[17px] border-b border-[#424245]">
+            <a href="/" className="flex items-center w-full h-full">
+              TV & Home
+            </a>
+          </li>
+          <li className="h-11 text-[17px] border-b border-[#424245]">
+            <a href="/" className="flex items-center w-full h-full">
+              Only on Apple
+            </a>
+          </li>{' '}
+          <li className="h-11 text-[17px] border-b border-[#424245]">
+            <a href="/" className="flex items-center w-full h-full">
+              Accessories
+            </a>
+          </li>{' '}
+          <li className="h-11 text-[17px]">
+            <a href="/" className="flex items-center w-full h-full">
+              Support
+            </a>
+          </li>
+        </ul>
       </div>
     </aside>
   );
