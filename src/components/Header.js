ddbf9c1e771c1ca3ce2a627c29e5eac920e93b29
yeonsuBaek <soothbugger@gmail.com>
2023-01-30 09:28:06 +0900

    feat: 버튼 클릭 시 Sidebar 토글 기능 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GlobalNav from './GlobalNav';
 import Sidebar from './Sidebar';
 
 function Header() {
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+
+  const handleToggleSidebar = () => {
+    setToggleSidebar(!toggleSidebar);
+  };
+
   return (
     <>
-      <nav className="w-full h-full bg-[#161617cc]/80 flex items-center justify-between lg:block lg:px-[22px]">
-        <ul className="flex items-center justify-between w-full h-12 lg:max-w-screen-lg lg:h-11 lg:my-0 lg:mx-auto">
-          <li className="flex items-center justify-center w-12 h-full text-center lg:visibility-hidden">
-            <a href="/" className="w-full h-full focus display">
-              <label className="relative block w-full h-full">
-                <span className="block w-[17px] h-px bg-white absolute top-5 left-[50%] translate-x-[-50%]"></span>
-                <span className="block w-[17px] h-px bg-white absolute bottom-5 left-[50%] translate-x-[-50%]"></span>
+      <nav className='w-full h-full bg-[#161617cc]/80 flex items-center justify-between lg:block lg:px-[22px]'>
+        <ul className='flex items-center justify-between w-full h-12 reletive lg:max-w-screen-lg lg:h-11 lg:my-0 lg:mx-auto'>
+          <li className='flex items-center justify-center w-12 h-full text-center lg:visibility-hidden'>
+            <button onClick={handleToggleSidebar} className='w-full h-full focus display'>
+              <label className='relative block w-full h-full'>
+                <span
+                  className={
+                    toggleSidebar
+                      ? 'rotate-45 block w-[17px] h-px bg-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ease-in duration-200'
+                      : 'rotate-0 block w-[17px] h-px bg-white absolute top-5 left-[50%] translate-x-[-50%] translate-y-0 ease-in duration-200'
+                  }
+                ></span>
+                <span
+                  className={
+                    toggleSidebar
+                      ? 'rotate-[-45deg] block w-[17px] h-px bg-white absolute bottom-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  ease-in duration-200'
+                      : 'rotate-0 block w-[17px] h-px bg-white absolute bottom-5 left-[50%] translate-x-[-50%] translate-y-0 ease-in duration-200'
+                  }
+                ></span>
               </label>
-              <span className="hidden">Global Navigation</span>
-            </a>
+              <span className='hidden'>Global Navigation</span>
+            </button>
           </li>
           <GlobalNav />
-          <li className="flex items-center justify-center w-12 h-full text-center">
+          <li className='flex items-center justify-center w-12 h-full text-center'>
             <a
-              href="/"
-              className="focus block w-[17px] h-full bg-[length:17px_48px] lg:bg-[length:15px_44px] bg-no-repeat bg-[url('https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_bag_image__yzte50i47ciu_small.svg')]"
+              href='/'
+              className={
+                toggleSidebar
+                  ? "translate-y-1/4 opacity-0 focus block w-[17px] h-full bg-[length:17px_48px] lg:bg-[length:15px_44px] bg-no-repeat bg-[url('https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_bag_image__yzte50i47ciu_small.svg')] ease-in duration-200 transition-all"
+                  : "translate-y-0 focus block w-[17px] h-full bg-[length:17px_48px] lg:bg-[length:15px_44px] bg-no-repeat bg-[url('https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_bag_image__yzte50i47ciu_small.svg')] ease-in duration-200 transition-translate"
+              }
             >
-              <span className="hidden">Shopping Bag</span>
+              <span className='hidden'>Shopping Bag</span>
             </a>
           </li>
         </ul>
       </nav>
 
-      <Sidebar />
+      <Sidebar toggle={toggleSidebar} />
     </>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,68 +1,74 @@
 import React from 'react';
 
-function Sidebar() {
+const Sidebar = (props) => {
   return (
-    <aside className="absolute left-0 w-screen h-[calc(100vh-48px)] bg-black top-12">
-      <div className="mb-1 px-2.5 h-[53px] w-full flex items-start justify-center border-b border-[#424245]">
-        <form className="relative mx-auto my-0 w-full max-w-[584px]">
+    <aside
+      className={
+        props.toggle
+          ? 'absolute top-12 left-0 w-screen h-[calc(100vh-48px)] bg-black ease-in duration-500 opacity-100'
+          : 'overflow-y-hidden lg:visibility-hidden ease-in duration-300 h-0 opacity-0'
+      }
+    >
+      <div className='mb-1 px-2.5 h-[53px] w-full flex items-start justify-center border-b border-[#424245]'>
+        <form className='relative mx-auto my-0 w-full max-w-[584px]'>
           <button className="focus absolute top-0 left-0 w-[30px] h-9 bg-no-repeat bg-left-[8px] bg-center bg-[url('https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_search_image__cbllq1gkias2_small.svg')]"></button>
           <input
-            type="text"
-            placeholder="Search apple.com"
+            type='text'
+            placeholder='Search apple.com'
             className="pl-[30px] rounded-lg w-full block h-9 bg-[#1d1d1f] outline-0 text-[17px] placeholder:font-['SF_Pro_Text'] leading-[1.23536] tracking-[0.022em] placeholder:text-[#6e6e73]"
           />
         </form>
       </div>
 
-      <div className="px-10">
-        <ul className="max-w-[584px] mx-auto my-0">
-          <li className="h-11 text-[17px] border-b border-[#424245]">
-            <a href="/" className="flex items-center w-full h-full">
+      <div className='px-10'>
+        <ul className='max-w-[584px] mx-auto my-0'>
+          <li className='h-11 text-[17px] border-b border-[#424245]'>
+            <a href='/' className='flex items-center w-full h-full'>
               Store
             </a>
           </li>
-          <li className="h-11 text-[17px] border-b border-[#424245]">
-            <a href="/" className="flex items-center w-full h-full">
+          <li className='h-11 text-[17px] border-b border-[#424245]'>
+            <a href='/' className='flex items-center w-full h-full'>
               Mac
             </a>
           </li>{' '}
-          <li className="h-11 text-[17px] border-b border-[#424245]">
-            <a href="/" className="flex items-center w-full h-full">
+          <li className='h-11 text-[17px] border-b border-[#424245]'>
+            <a href='/' className='flex items-center w-full h-full'>
               iPad
             </a>
           </li>
-          <li className="h-11 text-[17px] border-b border-[#424245]">
-            <a href="/" className="flex items-center w-full h-full">
+          <li className='h-11 text-[17px] border-b border-[#424245]'>
+            <a href='/' className='flex items-center w-full h-full'>
               iPhone
             </a>
           </li>
-          <li className="h-11 text-[17px] border-b border-[#424245]">
-            <a href="/" className="flex items-center w-full h-full">
+          <li className='h-11 text-[17px] border-b border-[#424245]'>
+            <a href='/' className='flex items-center w-full h-full'>
               Watch
             </a>
           </li>{' '}
-          <li className="h-11 text-[17px] border-b border-[#424245]">
-            <a href="/" className="flex items-center w-full h-full">
+          <li className='h-11 text-[17px] border-b border-[#424245]'>
+            <a href='/' className='flex items-center w-full h-full'>
               AirPods
             </a>
           </li>{' '}
-          <li className="h-11 text-[17px] border-b border-[#424245]">
-            <a href="/" className="flex items-center w-full h-full">
+          <li className='h-11 text-[17px] border-b border-[#424245]'>
+            <a href='/' className='flex items-center w-full h-full'>
               TV & Home
             </a>
           </li>
-          <li className="h-11 text-[17px] border-b border-[#424245]">
-            <a href="/" className="flex items-center w-full h-full">
+          <li className='h-11 text-[17px] border-b border-[#424245]'>
+            <a href='/' className='flex items-center w-full h-full'>
               Only on Apple
             </a>
           </li>{' '}
-          <li className="h-11 text-[17px] border-b border-[#424245]">
-            <a href="/" className="flex items-center w-full h-full">
+          <li className='h-11 text-[17px] border-b border-[#424245]'>
+            <a href='/' className='flex items-center w-full h-full'>
               Accessories
             </a>
           </li>{' '}
-          <li className="h-11 text-[17px]">
-            <a href="/" className="flex items-center w-full h-full">
+          <li className='h-11 text-[17px]'>
+            <a href='/' className='flex items-center w-full h-full'>
               Support
             </a>
           </li>
@@ -70,6 +76,6 @@ function Sidebar() {
       </div>
     </aside>
   );
-}
+};
 
 export default Sidebar;
