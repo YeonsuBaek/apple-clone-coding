0edb2e982a6f3e59921c08b1e66183b471cd2329
yeonsuBaek <soothbugger@gmail.com>
2023-01-31 08:27:56 +0900

    feat: 버튼 클릭 시 localbar 토글 기능 구현

--- src/components/Navigation/LocalNav.js
+++ src/components/Navigation/LocalNav.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LocalNav = () => {
+  const [toggleLocalbar, setToggleLocalbar] = useState(false);
+
+  const handleToggleLacalbar = () => {
+    setToggleLocalbar(!toggleLocalbar);
+  };
+
   return (
     <nav className='lg:max-w-screen-lg lg:mx-auto lg:my-0 flex items-center justify-between px-4 lg:px-0 h-12 border-b border-[#424245]'>
       <a href='/' className='text-white text-[19px] text-bold tracking-[0.228px]'>
@@ -8,9 +14,21 @@ const LocalNav = () => {
       </a>
 
       <div className='flex items-center justify-between'>
-        <button className='relative w-10 h-8 focus lg:visibility-hidden'>
-          <span className='absolute top-4 left-2.5 block w-[11px] h-px bg-white rotate-[40deg] rounded-l-[0.5px] '></span>
-          <span className='absolute top-4 right-2.5 block w-3 h-px bg-white rotate-[-40deg] rounded-r-[0.5px]'></span>
+        <button className='relative w-10 h-8 focus lg:visibility-hidden' onClick={handleToggleLacalbar}>
+          <span
+            className={
+              toggleLocalbar
+                ? 'absolute top-4 left-2.5 block w-[11px] h-px bg-white rotate-[-40deg] rounded-l-[0.5px] ease-in-out duration-500'
+                : 'absolute top-4 left-2.5 block w-[11px] h-px bg-white rotate-[40deg] rounded-l-[0.5px] ease-in-out duration-500'
+            }
+          ></span>
+          <span
+            className={
+              toggleLocalbar
+                ? 'absolute top-4 right-2.5 block w-3 h-px bg-white rotate-[40deg] rounded-r-[0.5px] ease-in-out duration-500'
+                : 'absolute top-4 right-2.5 block w-3 h-px bg-white rotate-[-40deg] rounded-r-[0.5px] ease-in-out duration-500'
+            }
+          ></span>
         </button>
 
         <ul className='flex max-[1023px]:visibility-hidden'>
