0d942d75e8fe26373b7749af6b14fb2e352b7740
yeonsuBaek <soothbugger@gmail.com>
2023-01-30 09:59:08 +0900

    fix: 삭제된 Search 돌려놓기

--- src/components/Navigation/Header.js
+++ src/components/Navigation/Header.js
@@ -86,6 +86,14 @@ function Header() {
             </a>
           </li>
           <GlobalNav navList={GlobalNavList} />
+          <li className='max-[1023px]:visibility-hidden h-full px-2'>
+            <a
+              href='/'
+              className="focus block w-[15px] h-full bg-[length:15px_88px] bg-no-repeat bg-[url('https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_search_image__cbllq1gkias2_large.svg')]"
+            >
+              <span className='hidden'>Search</span>
+            </a>
+          </li>
           <li className='flex items-center justify-center w-12 h-full text-center'>
             <a
               href='/'
