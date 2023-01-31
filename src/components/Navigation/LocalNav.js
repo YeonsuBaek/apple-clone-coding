ac05ec5347c3bb088a485bec586a69b3b7a45f69
yeonsuBaek <soothbugger@gmail.com>
2023-01-31 09:24:02 +0900

    feat: localNav가 토글할 때 트랜지션 효과 부여

--- src/components/Navigation/LocalNav.js
+++ src/components/Navigation/LocalNav.js
@@ -8,50 +8,99 @@ const LocalNav = () => {
   };
 
   return (
-    <nav className='lg:max-w-screen-lg lg:mx-auto lg:my-0 flex items-center justify-between px-4 lg:px-0 h-12 border-b border-[#424245]'>
-      <a href='/' className='text-white text-[19px] text-bold tracking-[0.228px]'>
-        iPhone 14 Pro
-      </a>
+    <nav
+      className={
+        toggleLocalbar
+          ? 'max-h-52 px-4 border-b border-[#424245] bg-zinc-900 ease-in-out duration-500 transition-all lg:max-w-screen-lg lg:mx-auto lg:my-0 lg:max-h-13 lg:bg-transparent lg:px-0'
+          : 'max-h-12 px-4 border-b border-[#424245] ease-in-out duration-500 delay-300 transition-all lg:max-w-screen-lg lg:mx-auto lg:my-0 lg:px-0 lg:max-h-13'
+      }
+    >
+      <div className='flex items-center justify-between h-12'>
+        <a href='/' className='text-white text-[19px] text-bold tracking-[0.228px]'>
+          iPhone 14 Pro
+        </a>
 
-      <div className='flex items-center justify-between'>
-        <button className='relative w-10 h-8 focus lg:visibility-hidden' onClick={handleToggleLacalbar}>
-          <span
-            className={
-              toggleLocalbar
-                ? 'absolute top-4 left-2.5 block w-[11px] h-px bg-white rotate-[-40deg] rounded-l-[0.5px] ease-in-out duration-500'
-                : 'absolute top-4 left-2.5 block w-[11px] h-px bg-white rotate-[40deg] rounded-l-[0.5px] ease-in-out duration-500'
-            }
-          ></span>
-          <span
-            className={
-              toggleLocalbar
-                ? 'absolute top-4 right-2.5 block w-3 h-px bg-white rotate-[40deg] rounded-r-[0.5px] ease-in-out duration-500'
-                : 'absolute top-4 right-2.5 block w-3 h-px bg-white rotate-[-40deg] rounded-r-[0.5px] ease-in-out duration-500'
-            }
-          ></span>
-        </button>
+        <div className='flex items-center justify-between'>
+          <button className='relative w-10 h-8 focus lg:visibility-hidden' onClick={handleToggleLacalbar}>
+            <span
+              className={
+                toggleLocalbar
+                  ? 'absolute top-4 left-2.5 block w-[11px] h-px bg-white rotate-[-40deg] rounded-l-[0.5px] ease-in-out duration-500'
+                  : 'absolute top-4 left-2.5 block w-[11px] h-px bg-white rotate-[40deg] rounded-l-[0.5px] ease-in-out duration-500'
+              }
+            ></span>
+            <span
+              className={
+                toggleLocalbar
+                  ? 'absolute top-4 right-2.5 block w-3 h-px bg-white rotate-[40deg] rounded-r-[0.5px] ease-in-out duration-500'
+                  : 'absolute top-4 right-2.5 block w-3 h-px bg-white rotate-[-40deg] rounded-r-[0.5px] ease-in-out duration-500'
+              }
+            ></span>
+          </button>
 
-        <ul className='flex max-[1023px]:visibility-hidden'>
-          <li className='mr-6 opacity-60'>
-            <a className='text-xs font-["SF_Pro_Text"] text-white cursor-default' href='/'>
+          <ul className='flex max-[1023px]:visibility-hidden'>
+            <li className='mr-6 opacity-60'>
+              <a className='text-xs font-["SF_Pro_Text"] text-white cursor-default' href='/'>
+                Overview
+              </a>
+            </li>
+            <li className='mr-6 opacity-90'>
+              <a
+                className='text-xs font-["SF_Pro_Text"] text-white hover:text-[#2997ff] active:text-[#2997ff]'
+                href='/'
+              >
+                Switching to iPhone
+              </a>
+            </li>
+            <li className='mr-6 opacity-90'>
+              <a
+                className='text-xs font-["SF_Pro_Text"] text-white hover:text-[#2997ff] active:text-[#2997ff]'
+                href='/'
+              >
+                Tech Specs
+              </a>
+            </li>
+          </ul>
+
+          <a href='/' className='px-3 py-1 ml-3 text-xs button lg:ml-0'>
+            Buy
+          </a>
+        </div>
+      </div>
+
+      <div
+        className={
+          toggleLocalbar
+            ? 'translate-y-0 max-h-full opacity-100 ease-in-out duration-500 delay-300 overflow-auto lg:visibility-hidden'
+            : 'translate-y-[-30px] max-h-0 opacity-0 ease-in-out duration-500 overflow-hidden'
+        }
+      >
+        <ul className='px-6 pt-1 pb-4'>
+          <li className='w-full mr-6 opacity-60 border-b border-[#424245]'>
+            <a
+              className='flex items-center justify-start h-11 text-sm font-["SF_Pro_Text"] text-white cursor-default'
+              href='/'
+            >
               Overview
             </a>
           </li>
-          <li className='mr-6 opacity-90'>
-            <a className='text-xs font-["SF_Pro_Text"] text-white hover:text-[#2997ff] active:text-[#2997ff]' href='/'>
+          <li className='w-full mr-6 opacity-90 border-b border-[#424245]'>
+            <a
+              className='flex items-center justify-start h-11 text-sm font-["SF_Pro_Text"] text-white hover:text-[#2997ff] active:text-[#2997ff]'
+              href='/'
+            >
               Switching to iPhone
             </a>
           </li>
-          <li className='mr-6 opacity-90'>
-            <a className='text-xs font-["SF_Pro_Text"] text-white hover:text-[#2997ff] active:text-[#2997ff]' href='/'>
+          <li className='w-full mr-6 opacity-90'>
+            <a
+              className='flex items-center justify-start h-11 text-sm font-["SF_Pro_Text"] text-white hover:text-[#2997ff] active:text-[#2997ff]'
+              href='/'
+            >
               Tech Specs
             </a>
           </li>
         </ul>
-
-        <a href='/' className='px-3 py-1 ml-3 text-xs button lg:ml-0'>
-          Buy
-        </a>
       </div>
     </nav>
   );
