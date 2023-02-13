2c88269918956b15552ac3cbafbe901e7b8f19f0
yeonsuBaek <soothbugger@gmail.com>
2023-02-13 12:41:50 +0900

    feat: HeroIntroduction 태블릿/데스크탑 버전 구현

--- src/components/Hero/HeroInformation.js
+++ src/components/Hero/HeroInformation.js
@@ -4,44 +4,42 @@ import { MdOutlineNavigateNext } from 'react-icons/md';
 
 const HeroInformation = () => {
   return (
-    <div className='relative h-[1620px] pb-[25vh] mt-[-25vh]'>
-      <div className='w-[324px] mx-auto my-0 sticky top-0 translate-y-[calc(50vh-50%+48px*0.5)] text-black z-30 font-semibold'>
-        <h2 className='pt-2 pb-3 text-7xl'>
+    <div className='relative h-[1720px] pb-[4vh] mt-[-25vh] md:h-[2352px] md:pb-[-4vh] md:mt-[-50vh]'>
+      <div className='w-[324px] mx-auto my-0 sticky top-0 translate-y-[calc(50vh-50%+48px*0.5)] text-black z-30 font-semibold md:w-[609px] lg:w-[823px]'>
+        <h2 className='pt-2 pb-3 text-7xl md:text-[148px] md:pb-9 md:ml-[-9px] lg:text-[200px] lg:ml-[-13px]'>
           Pro.
           <br /> Beyond.
         </h2>
-        <p className='mb-5 text-xl leading-[1.1]'>
-          iPhone 14 Pro and
-          <br />
-          iPhone 14 Pro Max
+        <p className='flex-col mb-5 text-xl leading-[1.1] md:flex-row md:text-2xl md:mb-6 lg:text-[32px] lg:mb-[38px]'>
+          <span>iPhone 14 Pro and&nbsp;</span>
+          <span>iPhone 14 Pro Max</span>
         </p>
-        <p className='mb-5 text-xl leading-[1.1]'>
-          From $41.62/mo. for 24 mo.
-          <br />
-          or $999 before trade-in*
+        <p className='flex-col mb-5 text-xl leading-[1.1] md:flex-row md:text-lg lg:text-[24px] lg:mb-[32px]'>
+          <span>From $41.62/mo. for 24 mo.&nbsp;</span>
+          <span>or $999 before trade-in*</span>
         </p>
-        <ul>
-          <li className='mb-3.5 text-xl'>
+        <ul className='flex flex-col text-xl md:flex-row md:gap-12 md:text-lg lg:text-2xl'>
+          <li className='mb-3.5'>
             <a href='/' className='flex items-center justify-start'>
               Watch the film <GrCirclePlay className='ml-1 font-bold' />
             </a>
           </li>
-          <li className='mb-3.5 text-xl'>
+          <li className='mb-3.5'>
             <a href='/' className='flex items-center justify-start'>
-              Watch the event <MdOutlineNavigateNext />{' '}
+              Watch the event <MdOutlineNavigateNext />
             </a>
           </li>
         </ul>
       </div>
-      <div className='absolute top-[25vh] z-10'>
-        <div className='w-full h-[810px] overflow-hidden'>
+      <div className='absolute top-[25vh] z-10 pb-[100px] bg-black md:top-[50vh]'>
+        <div className='w-screen h-[810px] overflow-hidden md:h-[1176px]'>
           <img
             src='https://www.apple.com/v/iphone-14-pro/c/images/overview/hero/hero_background__ep8j6kim532a_small.png'
             alt=''
             className='object-cover w-full h-full'
           />
         </div>
-        <div className='w-full h-[810px] overflow-hidden'>
+        <div className='w-screen h-[810px] overflow-hidden md:h-[1176px]'>
           <img
             src='https://www.apple.com/v/iphone-14-pro/c/images/overview/hero/hero_background__ep8j6kim532a_small.png'
             alt=''
