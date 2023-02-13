ba58e442cc8ecfad300885923f09d2d559eca7a3
yeonsuBaek <soothbugger@gmail.com>
2023-02-13 12:56:26 +0900

    fix: HeroInformation 배경 움직임 위치 수정

--- src/components/Hero/HeroInformation.js
+++ src/components/Hero/HeroInformation.js
@@ -4,7 +4,7 @@ import { MdOutlineNavigateNext } from 'react-icons/md';
 
 const HeroInformation = () => {
   return (
-    <div className='relative h-[1720px] pb-[4vh] mt-[-25vh] md:h-[2352px] md:pb-[-4vh] md:mt-[-50vh]'>
+    <div className='relative z-10 h-[1720px] pb-[4vh] mt-[-25vh] md:h-[2752px] md:mt-[-50vh] lg:h-[3736px]'>
       <div className='w-[324px] mx-auto my-0 sticky top-0 translate-y-[calc(50vh-50%+48px*0.5)] text-black z-30 font-semibold md:w-[609px] lg:w-[823px]'>
         <h2 className='pt-2 pb-3 text-7xl md:text-[148px] md:pb-9 md:ml-[-9px] lg:text-[200px] lg:ml-[-13px]'>
           Pro.
@@ -31,15 +31,15 @@ const HeroInformation = () => {
           </li>
         </ul>
       </div>
-      <div className='absolute top-[25vh] z-10 pb-[100px] bg-black md:top-[50vh]'>
-        <div className='w-screen h-[810px] overflow-hidden md:h-[1176px]'>
+      <div className='absolute top-[25vh] z-10 pb-[100px] bg-black md:top-[50vh] md:pb-[400px] lg:pb-[600px]'>
+        <div className='w-screen h-[810px] overflow-hidden md:h-[1176px] lg:h-[1568px]'>
           <img
             src='https://www.apple.com/v/iphone-14-pro/c/images/overview/hero/hero_background__ep8j6kim532a_small.png'
             alt=''
             className='object-cover w-full h-full'
           />
         </div>
-        <div className='w-screen h-[810px] overflow-hidden md:h-[1176px]'>
+        <div className='w-screen h-[810px] overflow-hidden md:h-[1176px] lg:h-[1568px]'>
           <img
             src='https://www.apple.com/v/iphone-14-pro/c/images/overview/hero/hero_background__ep8j6kim532a_small.png'
             alt=''
