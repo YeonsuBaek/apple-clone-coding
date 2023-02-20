50fd87e82e9b016af27a36ba19bcb6f0a3008a09
yeonsuBaek <soothbugger@gmail.com>
2023-02-20 16:02:03 +0900

    (#1) style: LocalNav를 화면 상단에 계속 유지

--- src/components/Navigation/LocalNav.js
+++ src/components/Navigation/LocalNav.js
@@ -11,8 +11,8 @@ const LocalNav = () => {
     <nav
       className={
         toggleLocalbar
-          ? 'max-h-52 px-4 border-b border-[#424245] bg-zinc-900 ease-in-out duration-500 transition-all lg:max-w-screen-lg lg:mx-auto lg:my-0 lg:max-h-13 lg:bg-transparent lg:px-0'
-          : 'max-h-12 px-4 border-b border-[#424245] bg-transparent ease-in-out duration-300 delay-500 transition-all lg:max-w-screen-lg lg:mx-auto lg:my-0 lg:px-[22px] lg:max-h-13'
+          ? 'z-40 sticky top-0 left-0 w-full max-h-52 px-4 border-b border-[#424245] bg-zinc-900 ease-in-out duration-500 transition-all lg:max-w-screen-lg lg:mx-auto lg:my-0 lg:max-h-13 lg:bg-transparent lg:px-0'
+          : 'z-40 sticky top-0 left-0 w-full max-h-12 px-4 border-b border-[#424245] bg-black ease-in-out duration-300 delay-500 transition-all lg:max-w-screen-lg lg:mx-auto lg:my-0 lg:px-[22px] lg:max-h-13'
       }
     >
       <div className='flex items-center justify-between h-12'>
