e4a40c8329cba554abd7062400951cb3e6993b23
yeonsuBaek <soothbugger@gmail.com>
2023-02-20 14:48:11 +0900

    feat: App 컴포넌트에 자식 컴포넌트 삽입

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import HeroHeader from './components/Hero/HeroHeader';
-import HeroInformation from './components/Hero/HeroInformation';
-import HeroIntroduction from './components/Hero/HeroIntroduction';
+import Colors from './components/Colors/Colors';
+import Hero from './components/Hero/Hero';
 import Header from './components/Navigation/Header';
 import LocalNav from './components/Navigation/LocalNav';
 
 const App = () => {
   return (
-    <>
+    <div className='relative'>
       <Header />
       <LocalNav />
       <main>
-        <HeroHeader />
-        <HeroIntroduction />
-        <HeroInformation />
+        <Hero />
+        <Colors />
       </main>
-    </>
+    </div>
   );
 };
 
