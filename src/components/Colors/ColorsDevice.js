1f86580471abfa7cd8ae6fb75d660c7a4c99f4a5
yeonsuBaek <soothbugger@gmail.com>
2023-02-21 15:09:35 +0900

    (#2) DeviceList 파일 분리

--- src/components/Colors/ColorsDevice.js
+++ src/components/Colors/ColorsDevice.js
@@ -1,30 +1,10 @@
 import React from 'react';
-
-const devices = [
-  {
-    color: 'Deep Purple',
-    image:
-      'https://www.apple.com/v/iphone-14-pro/c/images/overview/colors/gallery_deep_purple__du23dbfjl1km_small_2x.jpg',
-  },
-  {
-    color: 'Gold',
-    image: 'https://www.apple.com/v/iphone-14-pro/c/images/overview/colors/gallery_gold__e2kfk9zl5eie_small_2x.jpg',
-  },
-  {
-    color: 'Silver',
-    image: 'https://www.apple.com/v/iphone-14-pro/c/images/overview/colors/gallery_silver__eph35go3eiy6_small_2x.jpg',
-  },
-  {
-    color: 'Space Black',
-    image:
-      'https://www.apple.com/v/iphone-14-pro/c/images/overview/colors/gallery_space_black__ev5ncqabz7ma_small_2x.jpg',
-  },
-];
+import DeviceList from './DeviceList';
 
 const ColorsDevice = (props) => {
   return (
     <ul className='relative w-[242px] h-[598px] mx-auto my-0 md:w-[306px] md:h-[776px] md:mx-6 lg:w-[406px] lg:h-[1022px] lg:mx-12'>
-      {devices.map((device) => {
+      {DeviceList.map((device) => {
         return (
           <li className='absolute top-0 left-0 z-0' key={device.color}>
             <img
---
+++ src/components/Colors/DeviceList.js
@@ -0,0 +1,22 @@
+const DeviceList = [
+  {
+    color: 'Deep Purple',
+    image:
+      'https://www.apple.com/v/iphone-14-pro/c/images/overview/colors/gallery_deep_purple__du23dbfjl1km_small_2x.jpg',
+  },
+  {
+    color: 'Gold',
+    image: 'https://www.apple.com/v/iphone-14-pro/c/images/overview/colors/gallery_gold__e2kfk9zl5eie_small_2x.jpg',
+  },
+  {
+    color: 'Silver',
+    image: 'https://www.apple.com/v/iphone-14-pro/c/images/overview/colors/gallery_silver__eph35go3eiy6_small_2x.jpg',
+  },
+  {
+    color: 'Space Black',
+    image:
+      'https://www.apple.com/v/iphone-14-pro/c/images/overview/colors/gallery_space_black__ev5ncqabz7ma_small_2x.jpg',
+  },
+];
+
+export default DeviceList;
