f4bbabf460875f3604dafa85470f980c21537d41
yeonsuBaek <soothbugger@gmail.com>
2023-02-27 10:42:04 +0900

    style: Privacy 태블릿, 데스크톱 버전 구현

--- src/components/Colors/Colors.js
+++ src/components/Colors/Colors.js
@@ -8,7 +8,7 @@ const Colors = () => {
   const [color, setColor] = useState('Deep Purple');
 
   return (
-    <div className='relative z-20 w-[87.5%] mx-auto my-2 pb-[86px] md:pb-[103px] lg:pb-[135px]'>
+    <div className='relative z-20 w-[87.5%] mx-auto my-2 pb-[86px] md:w-[692px] md:pb-[103px] lg:w-[980px] lg:pb-[135px]'>
       <h2 className='min-[0px]:visibility-hidden'>Colors</h2>
       <div className='mb-[39px] md:flex md:flex-row-reverse md:items-start md:justify-center md:mb-24 lg:mb-[72px]'>
         <ColorOptions setColor={setColor} color={color} />
--- src/components/Colors/Privacy.js
+++ src/components/Colors/Privacy.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Privacy = () => {
   return (
-    <div className='border-t-2 border-[#6e6e73] py-6 flex justify-between basis-full'>
+    <div className='border-t-2 border-[#6e6e73] py-6 flex justify-between basis-full md:py-[18px]'>
       <figure
         loading='lazy'
         className='animate-[lock_2000ms_0ms_ease-in-out_forwards] w-[28px] h-[38px] bg-[length:224px_228px] bg-[url("https://www.apple.com/v/iphone-14-pro/c/images/overview/shared/icon_privacy__h1tub3ldvgae_medium_2x.png")]'
       ></figure>
-      <p className='max-w-[83.3%] text-[19px] leading-[23px] tracking-[0.228px]'>
+      <p className='p-0 max-w-[83.3%] basis-[83.3%] text-[19px] leading-[23px] tracking-[0.228px] md:text-[17px] lg:max-w-[41.6%] lg:basis-[41.6%] lg:text-[19px]'>
         iPhone is also designed from the ground up to protect your privacy and put you in control of what you share and
         who you share it with
       </p>
